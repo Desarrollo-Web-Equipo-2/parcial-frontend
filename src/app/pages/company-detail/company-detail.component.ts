bc6b89469aa4dedfd26bd336b57e898d9ec221e4
alfonsoLeandro <leandroalfonsoporley@gmail.com>
2023-07-05 19:33:31 -0300

    Added ability to delete company

--- src/app/pages/company-detail/company-detail.component.ts
+++ src/app/pages/company-detail/company-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CompanyService } from '../../services/company.service';
 import { Company } from '../../interfaces/company';
 
@@ -13,7 +13,8 @@ export class CompanyDetailComponent implements OnInit {
     company?: Company;
 
     constructor(private route: ActivatedRoute,
-                private companyService: CompanyService) {
+                private companyService: CompanyService,
+                private router: Router) {
         this.route.params.subscribe({
             next: (params) => {
                 console.log(params);
@@ -30,4 +31,12 @@ export class CompanyDetailComponent implements OnInit {
     ngOnInit() {
     }
 
+    delete() {
+        this.companyService.deleteCompanyById(this.company!.id).subscribe({
+            next: (company) => {
+                console.log(company);
+                this.router.navigate(['/companies']);
+            }
+        });
+    }
 }
